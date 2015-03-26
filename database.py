5e1a6d06a526f475b2e9575395e07963208a896f
elpenao <elpenao@gmail.com>
2015-03-25 21:42:50 -0400

    it works

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 # The sqlite3 module is used to work with the SQLite database.
 import sqlite3 as lite
+import pandas as pd
 
 cities = (('Las Vegas', 'NV'),
                     ('Atlanta', 'GA'))
@@ -16,5 +17,14 @@ with con:
   cur = con.cursor()
   cur.execute("DROP TABLE IF EXISTS weather")
   cur.execute("DROP TABLE IF EXISTS cities")
+  cur.execute("CREATE TABLE cities (name text, state text)")
+  cur.execute("CREATE TABLE weather (city text, year integer, warm_month text, cold_month text)")
   cur.executemany("INSERT INTO cities VALUES(?,?)", cities)
   cur.executemany("INSERT INTO weather VALUES(?,?,?,?)", weather)
+  cur.execute("SELECT city, state, warm_month FROM weather INNER JOIN cities on city = name")
+
+  rows = cur.fetchall()
+  cols = [desc[0] for desc in cur.description]
+  df = pd.DataFrame(rows, columns=cols)
+
+  print df['city'] + ", " + df['state'] + " is hottest in " + df['warm_month']
